690a173deb35cc0c84a10fe7bf952ecf73ea99c9
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-06-01 13:23:40 +0530

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+
+    alias v="vim"
+    alias n="nvim"
+    alias ls="exa --icons"
+    alias ll="exa -alh --icons"
+    alias tree="exa --tree --icons"
+    alias cat="bat"
+    alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+end
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+
+starship init fish | source
+fzf --fish | source
+zoxide init --cmd cd fish | source
+
+# Created by `pipx` on 2024-04-30 12:18:47
+set PATH $PATH /home/ayush/.local/bin
