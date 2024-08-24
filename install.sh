da19a848ffa779ddedafb4593065bc2a7a694f66
ayush-1801 <ayush23162@iiitd.ac.in>
2024-08-24 13:43:40 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg
 sudo pacman --noconfirm -S xmonad xmonad-contrib xmobar
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> INSTALLING NVIDIA DRIVERS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-# sudo pacman --noconfirm -S nvidia nvidia-lts nvidia-settings nvidia-utils
+sudo pacman --noconfirm -S nvidia nvidia-settings nvidia-utils
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> INSTALLING IMPORTANT PROGRAMS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-sudo pacman --noconfirm -S git vim tmux fish neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst firefox firefox-developer-edition discord xcolor thunar gvfs rofi ly
+sudo pacman --noconfirm -S git vim tmux fish neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst firefox firefox-developer-edition discord xcolor thunar gvfs rofi ly keyd paru starship
 
-# paru -S picom-ftlabs-git
+paru -S picom-ftlabs-git
+paru -S ttf-jetbrains-mono-nerd
+paru -S noto-fonts-emoji
+paru -S spotify
+paru -S visual-studio-code-bin
