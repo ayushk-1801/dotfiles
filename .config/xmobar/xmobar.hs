6743087adea5337589f4a648dd93efc110e59d5f
ayush-1801 <ayush23162@iiitd.ac.in>
2024-08-25 16:08:52 +0530

    Added disku to xmobar

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -19,5 +19,5 @@ Config { font = "JetBrainsMono Nerd Font, Bold, 16"
         , alignSep = "}{"
         -- , template = "%UnsafeStdinReader% }{ <fc=#6699CC> %disku% </fc><fc=#E23D28> %cpu% </fc><fc=#fabd2f> %memory% </fc><fc=#b8bb26> %battery% </fc><fc=#83a598> %date% </fc>"
         -- , template = "%UnsafeStdinReader% } %date% { %default:Master%| %cpu% | %memory% | %battery% "
-        , template = "%UnsafeStdinReader% }{ <fc=#16161D,#1ED55F>%music%</fc> <fc=#16161D,#E46876> VOL </fc><fc=#16161D,#D27E88> %default:Master% </fc> <fc=#16161D,#FF9E3B> CPU </fc><fc=#16161D,#DCA561> %cpu% </fc> <fc=#16161D,#957FB8> MEM </fc><fc=#16161D,#938AA9> %memory% </fc> <fc=#DCD7BA,#E82424> BAT </fc><fc=#DCD7BA,#C34043> %battery% </fc> <fc=#16161D,#658594> 󰥔 </fc><fc=#16161D,#7E9CD8> %date% </fc>"
+        , template = "%UnsafeStdinReader% }{ <fc=#16161D,#1ED55F>%music%</fc> <fc=#16161D,#E46876> VOL </fc><fc=#16161D,#D27E88> %default:Master% </fc> <fc=#16161D,#318CE7> DISK </fc><fc=#16161D,#6699CC> %disku% </fc> <fc=#16161D,#FF9E3B> CPU </fc><fc=#16161D,#DCA561> %cpu% </fc> <fc=#16161D,#957FB8> MEM </fc><fc=#16161D,#938AA9> %memory% </fc> <fc=#DCD7BA,#E82424> BAT </fc><fc=#DCD7BA,#C34043> %battery% </fc> <fc=#16161D,#658594> 󰥔 </fc><fc=#16161D,#7E9CD8> %date% </fc>"
 }
