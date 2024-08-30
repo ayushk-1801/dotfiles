3298115be77164753f26eea0c1aeb19a145913a1
ayush-1801 <ayush23162@iiitd.ac.in>
2024-08-31 00:36:39 +0530

    Few changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 if status is-interactive
     set fish_greeting
 
-    alias n="nvim"
-    alias v="NVIM_APPNAME=vimm nvim"
+    alias v="nvim"
     alias ls="exa --icons"
     alias ll="exa -lah --icons --color=always --group-directories-first -F"
     alias tree="exa --tree --icons"
