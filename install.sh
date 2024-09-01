4767dcee228d1bcadd66365f3f836ee53507246b
ayush-1801 <ayush23162@iiitd.ac.in>
2024-09-01 12:53:44 +0530

    Removed qutebrowser

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> INSTALLING NVIDIA DRIVERS <<<<<<<<<
 sudo pacman --noconfirm -S nvidia nvidia-settings nvidia-utils
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> INSTALLING IMPORTANT PROGRAMS <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-sudo pacman --noconfirm -S git vim tmux fish neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst firefox firefox-developer-edition discord xcolor thunar gvfs rofi ly keyd paru starship xclip
+sudo pacman --noconfirm -S git vim tmux fish neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst firefox firefox-developer-edition discord xcolor thunar gvfs rofi ly keyd paru starship xclip tldr
 
 paru -S picom-ftlabs-git
 paru -S ttf-jetbrains-mono-nerd
