936160872214cb5667ebee4a86589d53ec11a288
ayush-1801 <ayush23162@iiitd.ac.in>
2024-09-28 19:52:12 +0530

    fish -> zsh

--- .config/xmobar/xmobar.hs
+++ .config/xmobar/xmobar.hs
@@ -20,7 +20,7 @@ Config { font = "JetBrainsMono Nerd Font, Bold, 16"
         , sepChar = "%"
         , alignSep = "}{"
         -- , template = "%UnsafeStdinReader% }{ <fc=#6699CC> %disku% </fc><fc=#E23D28> %cpu% </fc><fc=#fabd2f> %memory% </fc><fc=#b8bb26> %battery% </fc><fc=#83a598> %date% </fc>"
-        -- , template = "%UnsafeStdinReader% } %date% { %default:Master%| %cpu% | %memory% | %battery% "
+        -- , template = "%UnsafeStdinReader% } %date% { VOL: %default:Master% | DISK: %disku% | CPU: %cpu% | MEM: %memory% | BAT: %battery% "
         -- , template = "%UnsafeStdinReader% }{ <fc=#16161D,#1ED55F>%music%</fc> <fc=#DCD7BA,#9F2B68> TODO </fc><fc=#DCD7BA,#AA336A> %todos% </fc> <fc=#16161D,#E46876> VOL </fc><fc=#16161D,#D27E88> %default:Master% </fc> <fc=#16161D,#318CE7> DISK </fc><fc=#16161D,#6699CC> %disku% </fc> <fc=#16161D,#DD571C> TEMP </fc><fc=#16161D,#FC6A03> %temp% </fc> <fc=#16161D,#FF9E3B> CPU </fc><fc=#16161D,#DCA561> %cpu% </fc> <fc=#16161D,#957FB8> MEM </fc><fc=#16161D,#938AA9> %memory% </fc> <fc=#DCD7BA,#E82424> BAT </fc><fc=#DCD7BA,#C34043> %battery% </fc> <fc=#16161D,#658594> 󰥔 </fc><fc=#16161D,#7E9CD8> %date% </fc>"
         , template = "%UnsafeStdinReader% }{ <fc=#16161D,#1ED55F>%music%</fc> <fc=#DCD7BA,#9F2B68> TODO </fc><fc=#DCD7BA,#AA336A> %todos% </fc> <fc=#16161D,#E46876> VOL </fc><fc=#16161D,#D27E88> %default:Master% </fc> <fc=#16161D,#318CE7> DISK </fc><fc=#16161D,#6699CC> %disku% </fc> <fc=#16161D,#FF9E3B> CPU </fc><fc=#16161D,#DCA561> %cpu% </fc> <fc=#16161D,#957FB8> MEM </fc><fc=#16161D,#938AA9> %memory% </fc> <fc=#DCD7BA,#E82424> BAT </fc><fc=#DCD7BA,#C34043> %battery% </fc> <fc=#16161D,#658594> 󰥔 </fc><fc=#16161D,#7E9CD8> %date% </fc>"
 }
--- install.sh
+++ install.sh
@@ -16,9 +16,11 @@ sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot xorg
 
 sudo pacman --noconfirm -S nvidia nvidia-settings nvidia-utils
 
-sudo pacman --noconfirm -S git vim tmux fish neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst discord xcolor thunar gvfs rofi keyd paru starship xclip tldr physlock
+sudo pacman --noconfirm -S git vim tmux zsh neovim kitty neofetch bashtop exa bat fd fzf lxappearance nitrogen dunst discord xcolor thunar gvfs rofi keyd paru starship xclip tldr physlock
 
 yay --noconfirm -S picom-ftlabs-git ttf-jetbrains-mono-nerd noto-fonts-emoji spotify stremio visual-studio-code-bin zen-browser-avx2-bin
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 if [ -d ~/dotfiles ]; then
     cd ~/dotfiles || exit
